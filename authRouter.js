d7f8ccc7b97672cfd7de7e8146ea5d8a259ef091
airfun <ahmedovravshanbek21@gmail.com>
2021-10-30 17:10:18 +0500

    logined

--- authRouter.js
+++ authRouter.js
@@ -7,31 +7,30 @@ const generateToken = (id, roles) => {
     const payload = {id, roles};
     return jwt.sign(payload, secret, {expiresIn: "24h"})
 };
-
-router.post('/register', [
-    check('username', 'cant be empty').notEmpty(),
-    check('password', 'parol 4 dan kop bosin').isLength({min: 4})
-], async function (req, res) {
-    try {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({message: 'oshibka registratsii', errors: errors.errors})
-        }
-
-        const {username, password} = req.body;
-        // const candidate = await User.findOne({username});
-        if (candidate) {
-            return res.status(400).json({message: 'uje est'})
-        }
-        // const userRoles = await Role.findOne({value: 'USER'});
-        // const user = new User({username, password, roles: [userRoles.value]});
-        // await user.save();
-        res.json('boladi')
-    } catch (e) {
-        console.log(e)
-
-    }
-});
+// router.post('/register', [
+//     check('username', 'cant be empty').notEmpty(),
+//     check('password', 'parol 4 dan kop bosin').isLength({min: 4})
+// ], async function (req, res) {
+//     try {
+//         const errors = validationResult(req);
+//         if (!errors.isEmpty()) {
+//             return res.status(400).json({message: 'oshibka registratsii', errors: errors.errors})
+//         }
+//
+//         const {username, password} = req.body;
+//         // const candidate = await User.findOne({username});
+//         if (candidate) {
+//             return res.status(400).json({message: 'uje est'})
+//         }
+//         // const userRoles = await Role.findOne({value: 'USER'});
+//         // const user = new User({username, password, roles: [userRoles.value]});
+//         // await user.save();
+//         res.json('boladi')
+//     } catch (e) {
+//         console.log(e)
+//
+//     }
+// });
 router.post('/login', async function (req, res) {
     try {
         const {username, password} = req.body;
@@ -39,17 +38,17 @@ router.post('/login', async function (req, res) {
         if (!user) {
             return res.status(400).json({message: 'bunday odam yoq'})
         }
-        const validPassword = bcrypt.compareSync(password, user.password);
-        if (!validPassword) {
+        if (password !== user.password) {
             return res.status(400).json({message: 'parol xata'})
         }
-        const token = generateToken(user._id, user.roles);
-        return res.json({token})
+        return res.status(200).json({message: 'kirovr'})
+        // const token = generateToken(user._id, user.roles);
+        // return res.json({token})
     } catch (e) {
+        res.status(400).json({message: 'catch error'})
         console.log(e)
     }
-    res.json({message: 'login'})
-
+    // res.json({message: 'login'})
 });
 router.get('/get', async function (req, res) {
     try {
